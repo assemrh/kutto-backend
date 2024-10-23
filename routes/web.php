603c064cba36d5568d811f7c005cb7862033e113
TM/Asım RAHAL <assem.rh@gmail.com>
2024-10-23 11:25:04 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, "index"]);
 
 Route::resource("pets", \App\Http\Controllers\PetController::class);
+Route::resource("home", \App\Http\Controllers\HomeController::class);
