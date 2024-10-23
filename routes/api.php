2106ed1f7657267876709d5306516487ad3ce563
assemrh <assem.rh@gmail.com>
2024-10-23 09:22:54 +0300

    install laravel api

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource("pets", \App\Http\Controllers\Api\PetController::class);
