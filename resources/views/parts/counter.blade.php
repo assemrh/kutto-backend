bcb4c3d4ac677726fda53cdde4e024c4728c38e1
TM/Asım RAHAL <assem.rh@gmail.com>
2024-10-23 16:35:47 +0300

    up

--- resources/views/parts/counter.blade.php
+++ resources/views/parts/counter.blade.php
@@ -1,38 +1,38 @@
-<section class="counter-area counter-bg" style="background-image: url('img/bg/counter_bg.jpg')">
+<section class="counter-area counter-bg" style="background-image: url('{{ asset('img/bg/counter_bg.jpg') }}')">
     <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-xl-6 col-lg-8">
-          <div class="counter-title text-center mb-65">
-            <h6 class="sub-title">Why Choose Us?</h6>
-            <h2 class="title">Best Service to Breeds Your Loved Dog Explore</h2>
-          </div>
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-8">
+                <div class="counter-title text-center mb-65">
+                    <h6 class="sub-title">Why Choose Us?</h6>
+                    <h2 class="title">Best Service to Breeds Your Loved Dog Explore</h2>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <div class="counter-item">
-            <h2 class="count"><span class="odometer">75</span>%</h2>
-            <p>dogs are first bred</p>
-          </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="counter-item">
+                    <h2 class="count"><span class="odometer">75</span>%</h2>
+                    <p>dogs are first bred</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="counter-item">
+                    <h2 class="count"><span class="odometer">259</span>+</h2>
+                    <p>Most dogs are first</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="counter-item">
+                    <h2 class="count"><span class="odometer">39</span>K</h2>
+                    <p>Dog Breeding</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="counter-item">
+                    <h2 class="count"><span class="odometer">45</span>+</h2>
+                    <p>Years Of History</p>
+                </div>
+            </div>
         </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <div class="counter-item">
-            <h2 class="count"><span class="odometer">259</span>+</h2>
-            <p>Most dogs are first</p>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <div class="counter-item">
-            <h2 class="count"><span class="odometer">39</span>K</h2>
-            <p>Dog Breeding</p>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <div class="counter-item">
-            <h2 class="count"><span class="odometer">45</span>+</h2>
-            <p>Years Of History</p>
-          </div>
-        </div>
-      </div>
     </div>
-  </section>
+</section>
