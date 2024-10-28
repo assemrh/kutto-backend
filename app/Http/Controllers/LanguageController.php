bc20e9c3defc677f033b021dd688a858f264dce5
TM/Asım RAHAL <assem.rh@gmail.com>
2024-10-28 12:35:29 +0300

    update

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 
 
         if (isset($locale) && in_array($locale, ['en', 'tr'])) {
-            $request->session()->put('locale', $locale);
+            Session::put('locale', $locale);
             App::setLocale($locale);
         }
         return back()->with('locale', $locale);
--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -16,14 +16,13 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $request->session()->regenerate();
-        if ($request->session()->has('locale')) {
-            $locale = session()->get('locale');
+        if (Session::has('locale')) {
+            $locale = Session::get('locale');
         } else {
             $locale = 'en';
         }
 
-        $request->session()->put('locale', $locale);
+        Session::put('locale', $locale);
         App::setLocale($locale);
 
         return $next($request);
