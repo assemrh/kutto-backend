2ec594b98eba4219a34907091463fb6d5f29409d
TM/Asım RAHAL <assem.rh@gmail.com>
2024-10-28 11:39:44 +0300

    fix bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->append(\Illuminate\Session\Middleware\StartSession::class);
-        $middleware->append(\App\Http\Middleware\SetLocale::class);
+        //$middleware->append(\Illuminate\Session\Middleware\StartSession::class);
+        $middleware->appendToGroup('web', \App\Http\Middleware\SetLocale::class);
 
     })
     ->withExceptions(function (Exceptions $exceptions) {
